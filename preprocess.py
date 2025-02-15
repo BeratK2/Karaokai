539613ac9f839e18e24ccc9b102818190eb259f0
BeratK2 <bales1801@gmail.com>
2025-02-15 14:16:25 -0500

    Setting up preprocess function

---
+++ preprocess.py
@@ -0,0 +1,12 @@
+import librosa
+import os
+import numpy as np 
+
+"""FUNCTION TO PERFORM SHORT-TIME FOURNIER TRANSFORM TO CONVERT INPUT AUDIO FILE TO OUTPUT MEL-SPECTROGRAM"""
+def preprocess(audio: str):
+    # --- Pass audio file to STFT function and assign output to mel_spectrogram ---
+    mel_spectrogram = "Spectrogram here" 
+    
+    # --- Retrun mel spectrogram ---
+    return mel_spectrogram
+
