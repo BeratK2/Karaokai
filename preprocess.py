bb5c8dd6519be4a7848c29ab630ccf977553b91d
Sagar <galasagar1998@gmail.com>
2025-02-17 21:52:36 -0500

    Audio Directory Processing into Spectrogram

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,20 @@
 import librosa
 import os
-import numpy as np 
 
+folder_path = "./Wavfiles"
 """FUNCTION TO PERFORM SHORT-TIME FOURNIER TRANSFORM TO CONVERT INPUT AUDIO FILE TO OUTPUT MEL-SPECTROGRAM"""
-def preprocess(audio: str):
-    # --- Pass audio file to STFT function and assign output to mel_spectrogram ---
-    mel_spectrogram = "Spectrogram here" 
-    
-    # --- Retrun mel spectrogram ---
-    return mel_spectrogram
+def preprocess(dir_path):
+    try:
+        for filename in os.listdir(folder_path):
+            print("Inside ==>", filename)
+            file_path = os.path.join(folder_path, filename)
+            print("Audio ==>",file_path)
+        # --- Pass audio file to STFT function and assign output to mel_spectrogram ---
+            audio, sr = librosa.load(file_path)
+            print("Audio ==>",audio)
+            # --- Retrun mel spectrogram ---
+    except Exception as e:
+        print("Error",e)
 
+
+preprocess(folder_path)
