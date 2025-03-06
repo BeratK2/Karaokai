039355294da792b4fd44a0d85463d3ab887b6944
BeratK2 <bales1801@gmail.com>
2025-03-05 20:47:12 -0500

    Finished preprocessing layer

--- preprocess.py
+++ preprocess.py
@@ -1,39 +1,25 @@
 import librosa
-import os
-import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
-folder_path = "./Wavfiles"
-"""FUNCTION TO PERFORM SHORT-TIME FOURNIER TRANSFORM TO CONVERT INPUT AUDIO FILE TO OUTPUT MEL-SPECTROGRAM"""
-def preprocess(dir_path):
-    try:
-        log_file = open("message.log","w")
-        sys.stdout = log_file
-        for filename in os.listdir(folder_path):
-            nested_path = folder_path + "/" + filename
-            print("Nested Path ==> ",nested_path)
-            for songFiles in os.listdir(nested_path):
-                print("Inside ==>", songFiles)                           # Filename inside songFiles (Example: bass.wav)
-                file_path = os.path.join(folder_path,filename, songFiles)
-                instrumentals = 0
-                vocals = ()
-                if 'vocals' in songFiles:
-                    vocals = vocals + librosa.load(file_path)
-                    print("Vocals ==>",vocals)
-                # elif 'vocals' in songFiles:
-                #     vocals = vocals + librosa.load(file_path)
-                #     print("Vocals ==>",vocals)
-                elif 'drums' or 'bass' or 'other' in songFiles:
-                    print("Inside bass,drums,other" , songFiles)
-                    if instrumentals == 0:
-                        instrumentals = librosa.load(file_path)
-                    else:
-                        instrumentals = instrumentals + librosa.load(file_path)
-                    print("Instrumentals ==>",instrumentals)
-                
-                print("File Path ==>",file_path)    
-        log_file.close()
-    except Exception as e:
-        print("Error",e)
+audio = r"./Wavfiles/Cristina Vane - So Easy/bass.wav"
 
+'''FUNCTION TO PREPROCESS AUDIO FILE AND TURN IT INTO MEL SPECTROGRAM'''
+def preprocess(audio):
+    # --- Perform STFT to convert audio to mel spectrogram S --- 
+    y, sr = librosa.load(audio, sr=None)
+    D = np.abs(librosa.stft(y))**2
+    S = librosa.feature.melspectrogram(S=D, sr=sr)
 
-preprocess(folder_path)
+    # --- Plot the spectrogram --- 
+    fig, ax = plt.subplots()
+    S_dB = librosa.power_to_db(S, ref=np.max)
+    img = librosa.display.specshow(S_dB, x_axis='time',
+                            y_axis='mel', sr=sr,
+                            fmax=8000, ax=ax)
+    fig.colorbar(img, ax=ax, format='%+2.0f dB')
+    ax.set(title='Mel-frequency spectrogram')
+    plt.show()
+    return S
+
+preprocess(audio)
